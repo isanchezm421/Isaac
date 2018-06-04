94514d44dda1d7f59138d97bb0330c13dab78e71
isanchezm421 <39276212+isanchezm421@users.noreply.github.com>
2018-06-04 12:00:30 -0600

    semana 4

---
+++ Primos/src/primos/Primos.java
@@ -0,0 +1,26 @@
+
+
+package primos;
+
+import java.util.Scanner;
+
+public class Primos {
+
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Digite un numero para saber si es primo: ");
+        int number = scanner.nextInt();
+        boolean isPrime = true;
+        
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        } 
+        
+        System.out.println("El numero " + (isPrime ? "si" : "no") + " es primo.");
+    }
+    
+}
