862482d5f3dad1ccfc74854efd83de8ce94840bf
isanchezm421 <39276212+isanchezm421@users.noreply.github.com>
2018-07-16 00:17:41 -0600

    ordenar

---
+++ extraclass/extraclass.06-25.geometry/Cuadrado/src/cuadrado/Base.java
@@ -0,0 +1,14 @@
+
+package cuadrado;
+
+
+public class Base {
+    public static void main(String [] args){ 
+        Cuadrado square= new Cuadrado(5);
+        square.Drawsquare();
+        
+        Cuadrado square2 = new Cuadrado(15);
+        square2.Drawsquare();
+    }
+    
+}
